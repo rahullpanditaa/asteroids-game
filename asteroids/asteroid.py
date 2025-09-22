a3ca56378667e49ae775d46c412e15ae20402b0b
Rahul Pandita <rpandita1999@outlook.com>
2025-09-22 06:59:04 +0000

    create Asteroid class; create method to draw an asteroid

---
+++ asteroids/asteroid.py
@@ -0,0 +1,9 @@
+from circleshape import CircleShape
+import pygame
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        return pygame.draw.circle(screen, "white", self.position, self.radius, 2)
