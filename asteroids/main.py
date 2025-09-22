40c8ac7576e4b053415153cd1418efebc5f3db10
Rahul Pandita <rpandita1999@outlook.com>
2025-09-22 05:58:49 +0000

    restrict screen redrawing to 60 fps

--- asteroids/main.py
+++ asteroids/main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock() # create a new clock object
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -10,6 +12,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        milliseconds = clock.tick(60)
+        dt = milliseconds / 1000
 
 
 if __name__ == "__main__":
