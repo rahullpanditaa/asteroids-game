7f38851a4a539132f27bfa98fed2e2ab3be66802
Rahul Pandita <rpandita1999@outlook.com>
2025-09-22 07:00:59 +0000

    override update method in Asteroids class to make asteroid object move in a straight line

--- asteroids/asteroid.py
+++ asteroids/asteroid.py
@@ -7,3 +7,6 @@ class Asteroid(CircleShape):
 
     def draw(self, screen):
         return pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
